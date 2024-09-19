2f277a1fa7cb983d07f8e768a6685dbbab59e22f
Iorgulescu Dragos <dragos@creativepill.com>
2024-09-19 14:08:42 +0100

    feat: add a seed for the recipe data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,37 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'json'
+
+file_path = Rails.root.join('db', 'recipes-en.json')
+recipes_data = JSON.parse(File.read(file_path))
+
+seeded_recipes_count = 0
+seeded_ingredients_count = 0
+recipes_data.each_batch(batch_size: ) do |recipe_data|
+  next if Recipe.exists?(title: recipe_data['title'])
+
+  recipe = Recipe.create!(
+    title: recipe_data['title'],
+    cook_time: recipe_data['cook_time'],
+    prep_time: recipe_data['prep_time'],
+    rating: recipe_data['ratings'],
+    cuisine: recipe_data['cuisine'],
+    category: recipe_data['category'],
+    author: recipe_data['author'],
+    image: recipe_data['image']
+  )
+
+  seeded_recipes_count += 1
+  puts "Created <#{recipe.title}> recipe"
+
+  recipe_data['ingredients'].each do |ingredient_name|
+    next if recipe.ingredients.exists?(name: ingredient_name)
+    ingredient = recipe.ingredients.create!(name: ingredient_name)
+
+    seeded_ingredients_count += 1
+    puts "Created <#{ingredient.name}> ingredient for <#{recipe.title}> recipe"
+  end
+end
+
+puts "Seeded #{seeded_recipes_count} recipes and #{seeded_ingredients_count} ingredients."
